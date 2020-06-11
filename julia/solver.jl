650513de437b86ae5bf18e7eb32deeddf632d611
Federico Lodovici <f.lodovici@ieee.org>
2020-06-11 19:51:35 +0200

    added julia

---
+++ julia/solver.jl
@@ -0,0 +1,24 @@
+using MatrixMarket
+using LinearAlgebra
+using CSV
+using DataFrames
+
+function systemSolver(A, xe)
+  b = A * xe
+  dA = cholesky(A)
+  x = dA \ b
+end
+
+cd("../matrixes/")
+files = readdir()
+for (index, file) in enumerate(files)
+  A = MatrixMarket.mmread(file)
+  xe = ones(Int,size(A)[1])
+  timing = @timed systemSolver(A,xe)
+  t = timing[2]
+  mem = timing[3] * (10^-6)
+  err = norm(timing[1]-xe)/norm(xe)
+  out = DataFrame(MatriName=file,Size=size(A)[1],MemoryUsage=mem,
+        Time=t,RelativeError=err)
+  CSV.write("../reports/Juliareport.csv", out,writeheader = (index==1), append=true)
+end
